8f9dc2c02a4cb3c877db37c88314c170175cfde8
Arnar Birgisson <arnarb@google.com>
2021-01-11 13:25:25 -0800

    Long overdue, lots of updates

--- xmonad.hs
+++ xmonad.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import XMonad
 import XMonad.Config.Desktop
 import XMonad.Util.EZConfig
@@ -5,145 +7,128 @@ import XMonad.Actions.CycleWS
 import XMonad.Actions.SwapWorkspaces
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.GridVariants
-import XMonad.Layout.IM
-import XMonad.Layout.Reflect
 import XMonad.Layout.TwoPane
-import qualified XMonad.Layout.Groups as G
-import XMonad.Layout.Groups.Helpers
-import XMonad.Layout.SimpleFloat
+import XMonad.Layout.Spacing
+import qualified XMonad.Layout.Gaps as Gaps
 import XMonad.Util.Run
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.FadeInactive
-import XMonad.Actions.WindowBringer
-import XMonad.Util.Loggers
---import XMonad.Actions.Commands
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.NoBorders
 import XMonad.Util.Scratchpad
-import Data.List
-import Data.Maybe
-import Data.Ratio ((%))
-import System.IO
-import Control.Monad
 import qualified XMonad.StackSet as S
 import XMonad.Hooks.Place
-import XMonad.Actions.CopyWindow
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ManageHelpers (doRectFloat)
 
-myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+import qualified Data.ByteString as B
+import Data.String (fromString)
 
-myLayout = smartBorders tiled
-       ||| noBorders Full
-       ||| smartBorders (TwoPane (3/100) (3/5))      -- Good for TeXing
-       ||| smartBorders (G.group (Grid (16/9)) Full) -- Good for editor fullscreen + bunch of terminals
-       ||| smartBorders (Grid (16/9))
-       ||| im
-       ||| gimp
-       ||| simpleFloat
-  where
-    tiled  = Tall nmaster delta ratio
-    nmaster = 1
-    ratio = 3/5
-    delta = 3/100
-    im = withIM (1%7) (Or (Title "Buddy List") (Title "arnarbi - Skype™")) (Grid (1/2))
-    gimp = withIM (0.11) (Role "gimp-toolbox") $
-           reflectHoriz $
-           withIM (0.15) (Role "gimp-dock") Full
+myWorkspaces = map show [1..9]
 
-myManageHook = (composeAll
-    [ title =? "Do" --> doIgnore
-    , resource =? "Do" --> doFloat
-    , resource =? "unity-2d-panel" --> doIgnore
-    , resource =? "spotify.exe" --> doFloat
-    , resource =? "hamster-applet" --> doFloat
-    , (className =? "Firefox" <&&> resource =? "Download") --> doFloat
-    , (className =? "Firefox" <&&> resource =? "Extension") --> doFloat
-    , className =? "Shutter" --> doFloat
-    --, (stringProperty "WM_WINDOW_ROLE" =? "pop-up") --> doFloat   -- e.g. the hangouts extension
-    , isHangouts --> placeHook hangoutsPlacement <+> doFloat
-    ])  <+> manageScratchPad
-  where
-    isHangouts = appName =? "crx_nckgahadagoaajjgafhacjanaoiihapd"
+myLayoutHook = spacingRaw True (Border 1 0 1 0) True (Border 0 1 0 1) True
+               $ desktopLayoutModifiers myLayout
+  where myLayout = narrowable (noBorders tiled)
+          ||| narrowable (noBorders Full)
+          ||| noBorders (TwoPane (3/100) (3/5))  -- One win always visible, rest stacked
+          ||| noBorders (Grid (16/9))
+        tiled  = Tall nmaster delta ratio
+        nmaster = 1
+        ratio = 3/5
+        delta = 3/100
+        narrowable = Gaps.gaps' [((Gaps.L,512),False),((Gaps.R,512),False)]
 
-hangoutsPlacement = withGaps (20,5,30,0) $ smart (1.0, 1.0)
-
-manageScratchPad = scratchpadManageHook (S.RationalRect l t w h)
-  where
-    h = 0.1
-    w = 1
-    t = 1 - h
-    l = 1 - w
-
-myKeys = concat [
-          [ ("M-x f",       spawn "firefox" ) 
-          , ("M-u",         prevWS )
-          , ("M-d",         nextWS )
-          , ("M-S-u",       shiftToPrev )
-          , ("M-S-d",       shiftToNext )
-          , ("M-S-g",       gotoMenu )
-          , ("M-S-b",       bringMenu )
-          , ("M-p",         spawn "dmenu_run -l 5 -nb dimgray -nf whitesmoke -sb darkgray -sf white" )
-          --, ("M-u",         scratchpadSpawnActionTerminal "gnome-terminal --disable-factory --name scratchpad" )
-          , ("M-S-C-j",     moveToGroupDown True)
-          , ("M-S-C-k",     moveToGroupUp True)
-          , ("M-S-C-h",     moveToNewGroupDown)
-          , ("M-S-C-l",     moveToNewGroupUp)
-          , ("M-x c",       placeFocused hangoutsPlacement)  -- place floated chat window
-	  --, ("M-a",         sequence_ $ [windows $ copy i | i <- XMonad.workspaces conf])
-	  --, ("M-S-a",       windows $ kill8)
-	  , ("M1-<KP_Add>", spawn "amixer -c 0 set Master 1+")
-	  , ("M1-<KP_Subtract>", spawn "amixer -c 0 set Master 1-")
-	  , ("<XF86PowerOff>", spawn "xsecurelock")
-          ],
-          [("M-C-" ++ i, windows $ swapWithCurrent i) | i <- myWorkspaces],
-	  [ (prefix ++ "M-" ++ ws, action ws)
-            | ws <- myWorkspaces
-	    , (prefix, action) <- [ ("", windows . S.view)
-	    			  , ("S-", windows . S.shift)]
-	  ]
-         ]
-
-kill8 ss | Just w <- S.peek ss = (S.insertUp w) $ S.delete w ss
-         | otherwise = ss
-
--- LogHook prettyprinter for dzen
-
-pixmaps = "/usr/local/google/home/arnarb/.xmonad/"
-light_gray = "#cccccc" -- #c7c8c6"
-dark_gray  = "#434541"  -- very very dark, was "#434541" before, #303030 to match vim notext
---font = "xft:Sans:size=9:weight=regular:hinting=true:hintstyle=hintslight:antialias=true:rgba=rgb:lcdfilter=lcdligh"
-font = "Ubuntu Mono-10"
---font = "Droid Sans Mono-10"
-
-myPP pipe =  defaultPP { ppOutput = hPutStrLn pipe
-                       , ppCurrent  = dzenColor "white" "#990000" . pad
-                       , ppVisible  = dzenColor "#CCCCCC" "#666666" . pad
-                       , ppHidden   = dzenColor "#e6e6e6" dark_gray . pad
-                       , ppHiddenNoWindows = dzenColor "#666666" dark_gray . pad
-                       , ppUrgent   = dzenColor "red" "yellow"
-                       , ppWsSep    = ""
-                       , ppSep      = ""
-                       , ppLayout   = dzenColor light_gray dark_gray . layoutPixmap
-                       , ppTitle    = dzenEscape . (":: " ++)
-                       {-, ppOrder    = \x -> case x of -}
-                       {-                       [a,b,c,d,e] -> [a,b,d,e,c]-}
-                       {-                       _ -> ["bzzt"]-}
-                       }
+myManageHook = manageScratchPad <+> (composeAll
+    [ resource =? "URxvtFuzzy" --> placeHook fzfCmdPlacement <+> hasBorder False <+> doFloat
+    , (stringProperty "WM_WINDOW_ROLE" =? "GtkFileChooserDialog") --> doRectFloat(S.RationalRect 0.25 0.25 0.5 0.5)
+    , resource =? "gnubby_ssh_prompt" --> doFloat
+    , resource =? "_Org_Capture_" --> placeHook capturePlacement <+> hasBorder False <+> doFloat
+    ])
+    where
+      fzfCmdPlacement = withGaps (20,0,20,0) $ fixed (0.5,0.6)
+      capturePlacement = withGaps (20,0,20,0) $ fixed (0.5,0.4)
+      manageScratchPad = scratchpadManageHook (S.RationalRect l t w h)
         where
-          layoutPixmap lname = case (lookup lname pixmapMap) of
-                                Just x -> " ^i(" ++ pixmaps ++ "layout-" ++ x ++ ".xbm) "
-                                Nothing -> pad lname
-          pixmapMap = [ ( "Tall"                  , "tall" )
-                      , ( "Mirror Tall"           , "mtall" )
-                      , ( "Full"                  , "full" )
-                      , ( "TwoPane"               , "twopane" )
-                      , ( "Grid"                  , "grid" )
-                      , ( "IM Grid"               , "im" )
-                      , ( "IM ReflectX IM Full"   , "gimp" )
-                      , ( "Grid by Full"          , "gridbyfull" )
-                      ]
+          h = 0.5
+          w = 1
+          t = 0
+          l = 1 - w
 
-myLogHook :: X ()
-myLogHook = fadeInactiveLogHook 0xdddddddd
+-- Carbon X1 function key row:
+--  XF86AudioMute
+--  XF86AudioLowerVolume
+--  XF86AudioRaiseVolume
+--  XF86AudioMicMute
+--  XF86MonBrightnessDown
+--  XF86MonBrightnessUp
+--  XF86Display
+--  XF86WLAN
+--  XF86Tools
+--  XF86Bluetooth
+--  XF86Favorites
+myKeys = concat [
+          [ ("M-p",                     spawn "~/bin/fuzzy_win ~/bin/fuzzy_cmd" )
+          , ("M-`",                     scratchpadSpawnActionTerminal "urxvtc" )
+          , ("M-y",                     swapNextScreen)
+          , ("M-c",                     spawn "emacsclient -ne '(make-capture-frame)'")
+          , ("<XF86PowerOff>",          screenlock)
+          , ("<XF86Favorites>",         screenlock)  -- Star key on thinkpad
+          , ("<XF86MonBrightnessDown>", spawn "~/tools/brightlight/brightlight -p -d 5")
+          , ("<XF86MonBrightnessUp>",   spawn "~/tools/brightlight/brightlight -p -i 5")
+          , ("<XF86AudioMute>",         spawn "amixer set Master toggle")
+          , ("<XF86AudioLowerVolume>",  spawn "amixer set Master 2%-")
+          , ("<XF86AudioRaiseVolume>",  spawn "amixer set Master 2%+")
+          , ("<XF86AudioMicMute>",      spawn "amixer set Capture toggle")
+          , ("<XF86Display>",           spawn "~/.dotfiles/resetscreens.sh")
+          , ("C-<Down>",                spawn "~/.dotfiles/resetscreens.sh")
+          , ("<Print>",                 spawn "flameshot gui")
+          , ("M-n",                     toggleGaps)
+          , ("M-S-h",                   incGaps)
+          , ("M-S-l",                   decGaps)
+          ],
+          [ (prefix ++ ws, action ws) | ws <- myWorkspaces
+                                      , (prefix, action) <- [ ("M-", windows . S.view)
+                                                            , ("S-M-", windows . S.shift)
+                                                            , ("C-M-", windows . swapWithCurrent)
+                                                            ]
+          ]
+         ]
+        where
+          toggleGaps = sendMessage Gaps.ToggleGaps
+          incGaps = sequence_ $ map (sendMessage . Gaps.IncGap 64) [Gaps.R, Gaps.L]
+          decGaps = sequence_ $ map (sendMessage . Gaps.DecGap 64) [Gaps.R, Gaps.L]
+          screenlock = spawn "\
+              \XSECURELOCK_BLANK_TIMEOUT=1 \
+              \XSECURELOCK_BLANK_DPMS_STATE=suspend \
+              \/usr/share/goobuntu-desktop-files/xsecurelock.sh"
+
+
+polylinePP = def { ppOutput = B.appendFile "/tmp/.xmonad-workspace-log" . fromString . (++ "\n")
+                 , ppCurrent = overline highlight . background highlight . pad , ppVisible = overline highlight . pad
+                 , ppHidden = pad . omit "NSP"
+                 , ppHiddenNoWindows = foreground (nord 3) . pad . omit "NSP"
+                 , ppUrgent = overline (nord 11) . pad
+                 , ppWsSep = ""
+                 --, ppSep = ":: \xF79F \xf66b \xfcb5 \xe795 \xe7a2 \xf120 \xf268 \xf7ae \xf7c0 \xfc56 \xfa6f \xf57f \xf017 \xf64f "
+                 , ppSep = ":: "
+                 , ppLayout = id
+                 , ppTitle = const ""
+                 }
+             where
+               omit nameToOmit x = if x == nameToOmit then "" else x
+               underline color s = concat [ "%{u", color, "}%{+u}", s, "%{u-}" ]
+               overline color s = concat [ "%{o", color, "}%{+o}", s, "%{o-}" ]
+               foreground color s = concat [ "%{F", color, "}", s, "%{F-}" ]
+               background color s = concat [ "%{B", color, "}", s, "%{B-}" ]
+               highlight = "#990000"
+               nord n = [ "#2e3440", "#3b4252", "#434c5e", "#4c566a", "#d8dee9", "#e5e9f0", "#eceff4", "#8fbcbb",
+                          "#88c0d0", "#81a1c1", "#5e81ac", "#bf616a", "#d08770", "#ebcb8b", "#a3be8c", "#b48ead" ] !! n
+
+
+myLogHook = do
+  dynamicLogWithPP polylinePP
+  -- For now using compton's 'inactive-fade', but if that stops working we can go back to this:
+  --fadeInactiveLogHook 0xeeeeeeee
 
 myStartupHook = do
   startupHook desktopConfig
@@ -152,20 +137,17 @@ myStartupHook = do
 myConfig = desktopConfig 
            { modMask = mod4Mask
            , workspaces = myWorkspaces
-           , layoutHook = desktopLayoutModifiers myLayout
+           , layoutHook = myLayoutHook
            , manageHook = manageHook desktopConfig <+> myManageHook
+           , handleEventHook = docksEventHook <+> handleEventHook desktopConfig
            , startupHook = myStartupHook
-           , normalBorderColor = "#505050"
-           , focusedBorderColor = "#660000"
-           , terminal = "urxvt"
+           , terminal = "urxvtc"
+           , logHook = myLogHook
            } `additionalKeysP` myKeys
 
 main = do
-  dzen <- spawnPipe ("~/tools/dzen/dzen2 -dock -xs 1 -h 20 -w 1600 -ta l "
-                     ++ "-fg '" ++ light_gray ++ "' -bg '" ++ dark_gray ++ "' "
-                     ++ "-fn '" ++ font ++ "'")
-  --dzen <- openFile "/usr/local/google/home/arnarb/.xmonad/logpipe" ReadWriteMode
-  --hSetBuffering dzen LineBuffering
-  xmonad $ myConfig {
-               logHook = myLogHook >> dynamicLogWithPP (myPP dzen)
-             }
+  safeSpawn "mkfifo" ["/tmp/.xmonad-workspace-log"]
+  xmonad $ withUrgencyHook NoUrgencyHook
+         $ docks
+         $ ewmh
+         $ myConfig
